195b70e89f532ef95a9ca9df46c660c19cc1d435
taryb <tbounavong@gmail.com>
2026-03-03 06:35:03 -0800

    test

---
+++ web/src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontFamily:
+          'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif',
+      }}
+    >
+      <h1 style={{ fontSize: '3rem', margin: 0 }}>hello</h1>
+    </div>
+  )
+}
+
+export default App
